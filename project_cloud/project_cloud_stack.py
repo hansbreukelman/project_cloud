8416f36b21d7d598ae925340f942b1766f94501e
hansbreukelman <107840904+hansbreukelman@users.noreply.github.com>
2022-12-15 15:25:49 +0100

    update

--- project_cloud/project_cloud_stack.py
+++ project_cloud/project_cloud_stack.py
@@ -2,15 +2,21 @@ from constructs import Construct
 from aws_cdk import (
     aws_ec2 as ec2,
     aws_s3 as s3,
+    aws_iam as iam,
+    aws_ssm as ssm,
     Stack,
 )
+
+with open("./user_data/user_data.sh") as f:
+    user_data = f.read()
 
 class ProjectCloudStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        #Webserver VPC
+        # ----------- Webserver VPC ----------- 
+
         vpc_webserver = ec2.Vpc(
             self, "VPC_1",
             ip_addresses=ec2.IpAddresses.cidr("10.10.10.0/24"),
@@ -24,47 +30,6 @@ class ProjectCloudStack(Stack):
                 ]
         )   
         
-        #Managementserver VPC
-        vpc_managementserver = ec2.Vpc(
-            self, "VPC_2",
-            ip_addresses=ec2.IpAddresses.cidr("10.20.20.0/24"),
-            max_azs=2,
-            nat_gateways=0,
-            subnet_configuration=[
-                ec2.SubnetConfiguration(
-                    name="public_man", 
-                    cidr_mask=26, 
-                    subnet_type=ec2.SubnetType.PUBLIC),
-                ]
-        )   
-
-        #VPC peering connection
-        VPC_Peering_connection = ec2.CfnVPCPeeringConnection(
-            self, "VPCPeeringConnection",
-            peer_vpc_id=vpc_managementserver.vpc_id,
-            vpc_id=vpc_webserver.vpc_id,
-        )
-
-        #Routing table for the adminserver
-        for subnet in vpc_managementserver.public_subnets:
-            ec2.CfnRoute(
-                self, 
-                id = f"{subnet.node.id} Managementserver Route Table",
-                route_table_id = subnet.route_table.route_table_id,
-                destination_cidr_block = "10.10.10.0/24", 
-                vpc_peering_connection_id = VPC_Peering_connection.ref,
-        )
-        
-        #Routing table for the webserver
-        for subnet in vpc_webserver.public_subnets:
-            ec2.CfnRoute(
-                self,
-                id = f"{subnet.node.id} Webserver Route Table",
-                route_table_id = subnet.route_table.route_table_id,
-                destination_cidr_block = "10.20.20.0/24", 
-                vpc_peering_connection_id = VPC_Peering_connection.ref,
-        )
-
         #--------------Webserver SG-------------------
         SG_webserver = ec2.SecurityGroup(self, "SGwebserver",
             vpc = vpc_webserver,
@@ -84,59 +49,109 @@ class ProjectCloudStack(Stack):
             ec2.Port.tcp(443),
         )
 
-        #--------------Managementserver SG-------------------
-        SG_managementserver = ec2.SecurityGroup(self, "SGmanagementserver",
-            vpc = vpc_managementserver,
-            security_group_name = "SGManServer",
-            allow_all_outbound = True,
-        )
-
-        #SSH traffic
-        SG_managementserver.add_ingress_rule(
+         #SSH traffic
+        SG_webserver.add_ingress_rule(
             ec2.Peer.any_ipv4(),
             ec2.Port.tcp(22),
         )
 
-        #RDP traffic
-        SG_managementserver.add_ingress_rule(
-            ec2.Peer.any_ipv4(),
-            ec2.Port.tcp(3389),
+        webserver_role = iam.Role(
+            self, 'webserver-role',
+            assumed_by = iam.ServicePrincipal('ec2.amazonaws.com'),
+            managed_policies = iam.ManagedPolicy.from_aws_managed_policy_name('AmazonS3ReadOnlyAccess'),
+        )
+        
+         # EC2 Web Server
+        instance_webserver = ec2.Instance(
+            self, 'webserver',
+            instance_type = ec2.InstanceType('t2.micro'),
+            machine_image = ec2.MachineImage.latest_amazon_linux(
+                generation = ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+                edition = ec2.AmazonLinuxEdition.STANDARD
+            ),
+            vpc = vpc_webserver,
+            security_group = SG_webserver,
+            key_name = 'ec2-key-pair', 
         )
 
-        #HTTP traffic
-        SG_managementserver.add_ingress_rule(
-            ec2.Peer.any_ipv4(),
-            ec2.Port.tcp(80),
-        )
+        # # ----------- Managementserver VPC -----------
 
-        #HTTPS traffic
-        SG_managementserver.add_ingress_rule(
-            ec2.Peer.any_ipv4(),
-            ec2.Port.tcp(443),
-        )
+        # vpc_managementserver = ec2.Vpc(
+        #     self, "VPC_2",
+        #     ip_addresses=ec2.IpAddresses.cidr("10.20.20.0/24"),
+        #     max_azs=2,
+        #     nat_gateways=0,
+        #     subnet_configuration=[
+        #         ec2.SubnetConfiguration(
+        #             name="public_man", 
+        #             cidr_mask=26, 
+        #             subnet_type=ec2.SubnetType.PUBLIC),
+        #         ]
+        # )   
 
-        # #S3 Bucket
-        # self.s3bucket = s3.Bucket(
-        # self, 'userdata_client',
-        # bucket_name = "bucket-for-userdata",
-        # encryption=s3.BucketEncryption.S3_MANAGED,
-        # versioned=True,
-        # enforce_ssl=True,
+        # #VPC peering connection
+        # VPC_Peering_connection = ec2.CfnVPCPeeringConnection(
+        #     self, "VPCPeeringConnection",
+        #     peer_vpc_id=vpc_managementserver.vpc_id,
+        #     vpc_id=vpc_webserver.vpc_id,
         # )
 
-        # # ------------Servers--------------
-
-        # # EC2 Web Server
-        # instance_webserver = ec2.Instance(self, 'webserver',
-        #     instance_type = ec2.InstanceType('t2.micro'),
-        #     machine_image = ec2.MachineImage.latest_amazon_linux(
-        #         generation = ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
-        #         edition = ec2.AmazonLinuxEdition.STANDARD
-        #     ),
-        #     vpc = vpc_webserver,
-        #     security_group = SG_webserver,
-                
+        # #Routing table for the adminserver
+        # for subnet in vpc_managementserver.public_subnets:
+        #     ec2.CfnRoute(
+        #         self, 
+        #         id = f"{subnet.node.id} Managementserver Route Table",
+        #         route_table_id = subnet.route_table.route_table_id,
+        #         destination_cidr_block = "10.10.10.0/24", 
+        #         vpc_peering_connection_id = VPC_Peering_connection.ref,
         # )
+        
+        # #Routing table for the webserver
+        # for subnet in vpc_webserver.public_subnets:
+        #     ec2.CfnRoute(
+        #         self,
+        #         id = f"{subnet.node.id} Webserver Route Table",
+        #         route_table_id = subnet.route_table.route_table_id,
+        #         destination_cidr_block = "10.20.20.0/24", 
+        #         vpc_peering_connection_id = VPC_Peering_connection.ref,
+        # )
+
+       
+
+        # #--------------Managementserver SG-------------------
+        # SG_managementserver = ec2.SecurityGroup(self, "SGmanagementserver",
+        #     vpc = vpc_managementserver,
+        #     security_group_name = "SGManServer",
+        #     allow_all_outbound = True,
+        # )
+
+        # #SSH traffic
+        # SG_managementserver.add_ingress_rule(
+        #     ec2.Peer.any_ipv4(),
+        #     ec2.Port.tcp(22),
+        # )
+
+        # #RDP traffic
+        # SG_managementserver.add_ingress_rule(
+        #     ec2.Peer.any_ipv4(),
+        #     ec2.Port.tcp(3389),
+        # )
+
+        # #HTTP traffic
+        # SG_managementserver.add_ingress_rule(
+        #     ec2.Peer.any_ipv4(),
+        #     ec2.Port.tcp(80),
+        # )
+
+        # #HTTPS traffic
+        # SG_managementserver.add_ingress_rule(
+        #     ec2.Peer.any_ipv4(),
+        #     ec2.Port.tcp(443),
+        # )
+
+        # # # ------------Servers--------------
+
+       
 
         # # EC2 Admin / Management Server
         # instance_managementserver = ec2.Instance(self, 'adminserver',
@@ -146,6 +161,15 @@ class ProjectCloudStack(Stack):
         #         ),
         #     vpc = vpc_managementserver,
         #     security_group = SG_managementserver,
+        # )
+
+    # #S3 Bucket
+        # s3bucket = s3.Bucket(
+        # self, 'userdata_client_test',
+        # bucket_name = "bucket-for-userdata",
+        # encryption=s3.BucketEncryption.S3_MANAGED,
+        # versioned=True,
+        # enforce_ssl=True,
         # )
 
             # Back up vault removal
