a50427c81297d5a3360918887afbde23ab0fbe50
hansbreukelman <107840904+hansbreukelman@users.noreply.github.com>
2022-12-20 16:25:05 +0100

    new

--- user_data/user_data.sh
+++ user_data/user_data.sh
@@ -4,4 +4,5 @@ sudo yum update
 sudo yum -y install httpd
 sudo systemctl enable httpd
 sudo systemctl start httpd
-sudo chmod 755 -R /var/www/html/
+sudo mkdir -p $(dirname '/var/www/html/user_data.sh')
+sudo chmod 755 -R /var/www/html/user_data.sh
