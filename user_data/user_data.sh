10c6bb2f1a754a22b76346d6c9b842ad34b098da
hansbreukelman <107840904+hansbreukelman@users.noreply.github.com>
2023-01-12 12:59:38 +0100

    yo

--- user_data/user_data.sh
+++ user_data/user_data.sh
@@ -2,7 +2,7 @@
 
 sudo yum update -y
 sudo yum install -y httpd
-sudo service httpd start
-sudo service httpd enable
-# sudo systemctl enable httpd
-# sudo systemctl start httpd
+# sudo service httpd start
+# sudo service httpd enable
+sudo systemctl enable httpd
+sudo systemctl start httpd
